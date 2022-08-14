48e08ba6e5e527c5cbb9fea77e902c563959d2b5
SantGT5 <gianspf@gmail.com>
2022-08-14 19:07:20 +0200

    Http Delete api

--- server/src/routers/launches/launches.controller.js
+++ server/src/routers/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.modal');
+const {
+    getAllLaunches,
+    addNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById,
+} = require('../../models/launches.modal');
 
 function HttpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
@@ -8,10 +13,10 @@ function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
     if (
-        !launch.mission.trim() ||
-        !launch.rocket.trim() ||
-        !launch.launchDate.trim() ||
-        !launch.destination.trim()
+        !launch.mission ||
+        !launch.rocket ||
+        !launch.launchDate ||
+        !launch.target
     ) {
         res.status(400).json({
             error: 'Mission required launch property',
@@ -30,7 +35,21 @@ function httpAddNewLaunch(req, res) {
     return;
 }
 
+function httpAbortLaunch(req, res) {
+    const { id } = req.params;
+    
+    if (!existsLaunchWithId(Number(id))) {
+        return res.status(404).json({
+            error: 'Launch not found',
+        });
+    }
+
+    const aborted = abortLaunchById(Number(id));
+    return res.status(200).json(aborted);
+}
+
 module.exports = {
     HttpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 };
--- server/src/routers/launches/launches.router.js
+++ server/src/routers/launches/launches.router.js
@@ -4,9 +4,11 @@ const launchesRouter = express.Router();
 const {
     HttpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 } = require('./launches.controller');
 
 launchesRouter.get('/launches', HttpGetAllLaunches);
 launchesRouter.post('/launches', httpAddNewLaunch);
+launchesRouter.delete('/launches/:id', httpAbortLaunch);
 
 module.exports = launchesRouter;
